c18b3e8cb7980d60dfd3a2fbbe22b05ff96bf4bf
kimmeskamp <26922235+kimmeskamp@users.noreply.github.com>
2020-03-19 18:33:45 +0100

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 /**
  * Niki, der Roboter
  */
-//% weight=100 color=#0fbc11
+//% weight=100 color=#0fbc11 icon="\uf14e"
 
 namespace niki {
 
@@ -16,256 +16,295 @@ namespace niki {
         westen = 3,
     }
 
-    let y = 0
-    let x = 0
-    let richtung = 0
-    let vorrat = 0
-    let geschwindigkeit = 0
-    let eingeschaltet = false
+    let x = 0 // Nikis x-Position auf dem Spielfeld
+    let y = 0 // Nikis y-Position auf dem Spielfeld
+    let richtung = 0 // Richtung, in die Niki gerade guckt
+    let vorrat = 0 // Nikis aktueller Vorrat an Schrauben
+    let verzoegerung = 0 // Millisekunden, die zwischen jeder von Nikis Aktionen vergehen sollen
+    let eingeschaltet = false // ist Niki eingeschaltet?
+    let fehler = false // ist ein Fehler eingetreten?
 
-    let spielfeld: number[][] = [[0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0]]
+    let spielfeld: number[][] = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+
 
     /**
-    * Erstelle Nikis Spielfeld und setze die im String angegebene Zahl von Token (0..9) auf die einzelnen Felder (Beginnend oben links). Beispiel: "1000011000111001111011111"
-    */
-    //% block
+     * Erstelle ein leeres Spielfeld. Niki darf nicht eingeschaltet sein.
+     */
+    //% blockId=erstelleSpielfeldLeer block="erstelle leeres Spielfeld"
+    export function erstelleSpielfeldLeer() {
+        if (!eingeschaltet) {
+            for (let i = 0; i < 5; i++) {
+                for (let j = 0; j < 5; j++) {
+                    spielfeld[0][0] = 0
+                }
+            }
+            basic.clearScreen()
+        } else {
+            fehler = true
+            basic.showIcon(IconNames.Sad)
+        }
+    }
+
+    /**
+     * Erstelle ein Spielfeld und setze die im String angegebene Zahl von Schrauben (0..9) auf die einzelnen Felder (Beginnend oben links). Beispiel: "1000011000111001111011111". Niki darf nicht eingeschaltet sein.
+     */
+    //% blockId=erstelleSpielfeld block="erstelle Spielfeld %s"
     export function erstelleSpielfeld(s: string) {
-        for (let i = 0; i < 5; i++) {
-            for (let j = 0; j < 5; j++) {
-                let n = parseInt(s.charAt(i + 5 * j))
-                if (n >= 0) {
-                    spielfeld[i][j] = n
+        if (!eingeschaltet) {
+            for (let i = 0; i < 5; i++) {
+                for (let j = 0; j < 5; j++) {
+                    let n = parseInt(s.charAt(i + 5 * j))
+                    if (n >= 0) {
+                        spielfeld[i][j] = n
+                    }
                 }
             }
-        }
 
-        for (let i = 0; i < 5; i++) {
-            for (let j = 0; j < 5; j++) {
-                if (spielfeld[i][j] > 0) {
-                    led.plot(i, j)
+            basic.clearScreen()
+            for (let i = 0; i < 5; i++) {
+                for (let j = 0; j < 5; j++) {
+                    if (spielfeld[i][j] > 0) {
+                        led.plot(i, j)
+                    }
                 }
             }
+        } else {
+            fehler = true
+            basic.showIcon(IconNames.Sad)
         }
     }
 
     /**
-    * Erstelle ein Spielfeld mit einer zufälliger Verteilung von Token für Niki.
-    */
-    //% block
+     * Erstelle ein Spielfeld mit einer zufälliger Verteilung. Etwa ein Fünftel der Felder wird mit einer Schraube belegt. Niki darf nicht eingeschaltet sein.
+     */
+    //% blockId=erstelleSpielfeldZufall block="erstelle zufälliges Spielfeld"
     export function erstelleSpielfeldZufall() {
-        for (let i = 0; i < 5; i++) {
-            for (let j = 0; j < 5; j++) {
-                if (Math.random(5) == 0) {
-                    spielfeld[i][j] = 1
-                } else {
-                    spielfeld[i][j] = 0
+        if (!eingeschaltet) {
+            for (let i = 0; i < 5; i++) {
+                for (let j = 0; j < 5; j++) {
+                    if (Math.random(5) == 0) {
+                        spielfeld[i][j] = 1
+                    } else {
+                        spielfeld[i][j] = 0
+                    }
                 }
             }
-        }
 
-        for (let i = 0; i < 5; i++) {
-            for (let j = 0; j < 5; j++) {
-                if (spielfeld[i][j] > 0) {
-                    led.plot(i, j)
+            basic.clearScreen()
+            for (let i = 0; i < 5; i++) {
+                for (let j = 0; j < 5; j++) {
+                    if (spielfeld[i][j] > 0) {
+                        led.plot(i, j)
+                    }
                 }
             }
+        } else {
+            fehler = true
+            basic.showIcon(IconNames.Sad)
         }
     }
 
     /**
-    * Setzt Niki auf die Koordinaten 0,0 mit Blick nach Norden.
-    */
-    //% block
+     * Setze Niki auf die Koordinaten x = 4, y = 4 mit Blick nach Norden. Niki darf nicht bereits eingeschaltet sein.
+     */
+    //% blockId=einschalten block="schalte ein"
     export function einschalten(): void {
         if (!eingeschaltet) {
-            x = 0;
-            y = 0;
+            x = 4;
+            y = 4;
             richtung = 0;
             led.plot(x, y)
             eingeschaltet = true
+            fehler = false
+        } else {
+            fehler = true
+            basic.showIcon(IconNames.Sad)
         }
     }
 
     /**
-    * Setzt Niki auf zufällige Koordinaten mit Blick in eine zufällige Richtung.
-    */
-    //% block
-    export function einschaltenZufall(): void {
+     * Setze Niki auf die angegebenen Koordinaten mit Blick in die angegebene Richtung. Niki darf nicht bereits eingeschaltet sein.
+     */
+    //% blockId=einschaltenPosition block="schalte ein mit x = %x| , y = %y| und Blick nach %richtung"
+    export function einschaltenPosition(x_neu: number, y_neu: number, richtung_neu: himmelsrichtungen): void {
         if (!eingeschaltet) {
-            x = Math.random(5)
-            y = Math.random(5)
-            richtung = Math.random(5)
-            led.plot(x, y)
-            eingeschaltet = true
+            fehler = false
+            if (x_neu >= 0 && x_neu < 5) {
+                x = x_neu
+            } else {
+                fehler = true
+            }
+            if (y_neu >= 0 && y_neu < 5) {
+                y = y_neu
+            } else {
+                fehler = true
+            }
+            if (richtung_neu >= 0 && richtung_neu < 4) {
+                richtung = richtung_neu
+            } else {
+                fehler = true
+            }
+
+            if (!fehler) {
+                led.plot(x, y)
+                eingeschaltet = true
+            } else {
+                basic.showIcon(IconNames.Sad)
+            }
+        } else {
+            fehler = true
+            basic.showIcon(IconNames.Sad)
         }
     }
 
     /**
-    * Setzt Niki auf die angegebenen Koordinaten mit Blick in die angegebene Richtung.
-    */
-    //% block
-    export function einschaltenPosition(x: number, y: number, richtung: himmelsrichtungen): void {
-        if (!eingeschaltet) {
-            if (x >= 0 && x < 5) {
-                x = x
-            }
-            if (y >= 0 && y < 5) {
-                y = y
-            }
-            if (richtung >= 0 && richtung < 4) {
-                richtung = richtung
-            }
-            led.plot(x, y)
-
-            eingeschaltet = true
-        }
-    }
-
-    /**
-    * Gehe, soweit möglich, ein Feld in die aktuelle Richtung.
-    */
-    //% block
+     * Gehe in der aktuellen Richtung ein Feld vor. Niki muss eingeschaltet sein und darf nicht vor eine Wand laufen.
+     */
+    //% blockId=geheVor block="gehe vor"
     export function geheVor(): void {
         if (eingeschaltet) {
-            basic.pause(geschwindigkeit)
+            basic.pause(verzoegerung)
 
             if (spielfeld[x][y] == 0) {
                 led.unplot(x, y)
             }
 
             if (richtung == 0 && y > 0) {
-                y += -1
-            } else if (richtung == 1 && x > 0) {
-                x += -1
+                y -= 1
+            } else if (richtung == 1 && x < 4) {
+                x += 1
             } else if (richtung == 2 && y < 4) {
                 y += 1
-            } else if (richtung == 3 && x < 4) {
-                x += 1
+            } else if (richtung == 3 && x > 0) {
+                x -= 1
+            } else {
+                fehler = true
             }
 
-            led.plot(x, y)
+            if (!fehler) {
+                led.plot(x, y)
+            } else {
+                basic.showIcon(IconNames.Sad)
+                ausschalten()
+            }
+
         } else {
             basic.showIcon(IconNames.Sad)
         }
     }
 
     /**
-    * Drehe dich um 90° nach links.
-    */
-    //% block
+     * Drehe dich um 90° nach links. Niki muss eingeschaltet sein.
+     */
+    //% blockId=dreheLinks block="drehe links"
     export function dreheLinks(): void {
         if (eingeschaltet) {
-            basic.pause(geschwindigkeit)
-            if (richtung < 3) {
-                richtung += 1
+            basic.pause(verzoegerung)
+            if (richtung > 0) {
+                richtung -= 1
             } else {
-                richtung = 0
+                richtung = 3
             }
         } else {
+            fehler = true
             basic.showIcon(IconNames.Sad)
         }
     }
 
-    /**
-    * Liefere "true", falls Niki nicht unmittelbar vor dem Bildschirmrand steht (in der aktuellen Richtung).
-    */
-    //% block
-    export function vorneFrei(): boolean {
-        if (richtung == 0 && y > 0) {
-            return true
-        } else if (richtung == 1 && x > 0) {
-            return true
-        } else if (richtung == 2 && y < 4) {
-            return true
-        } else if (richtung == 3 && x < 4) {
-            return true
-        } else {
-            return false
-        }
-    }
-
-    /**
-    * Liefere "true", falls links von Niki nicht unmittelbar der Bildschirmrand ist.
-    */
-    //% block
-    export function linksFrei(): boolean {
-        if (richtung == 0 && x > 0) {
-            return true
-        } else if (richtung == 1 && y < 4) {
-            return true
-        } else if (richtung == 2 && x < 4) {
-            return true
-        } else if (richtung == 3 && y > 0) {
-            return true
-        } else {
-            return false
-        }
-    }
-
-    /**
-    * Liefere "true", falls rechts von Niki nicht unmittelbar der Bildschirmrand ist.
-    */
-    //% block
-    export function rechtsFrei(): boolean {
-        if (richtung == 0 && x < 4) {
-            return true
-        } else if (richtung == 1 && y > 0) {
-            return true
-        } else if (richtung == 2 && x > 0) {
-            return true
-        } else if (richtung == 3 && y < 4) {
-            return true
-        } else {
-            return false
-        }
-    }
-
-    /**
-    * Liefert "true", wenn Niki nach Norden blickt.
-    */
-    //% block
-    export function nordwaerts(): boolean {
-        return (richtung == 0)
-    }
-
-    /**
-    * Liefert "true", wenn Niki nach Westen blickt.
-    */
-    //% block
-    export function westwaerts(): boolean {
-        return (richtung == 1)
-    }
-
-    /**
-    * Liefert "true", wenn Niki nach Süden blickt.
-    */
-    //% block
-    export function suedwaerts(): boolean {
-        return (richtung == 2)
-    }
-
-    /**
-    * Liefert "true", wenn Niki nach Osten blickt.
-    */
-    //% block
-    export function ostwaerts(): boolean {
-        return (richtung == 3)
-    }
-
-    /**
-     * Nimm ein Token von der aktuellen Position auf (sofern dort eines liegt). Bei mehreren Token auf der Position wird nur eines aufgehoben.
+	/**
+     * Liefert "true", falls Niki nicht unmittelbar vor dem Bildschirmrand steht (in der aktuellen Richtung). Niki muss eingeschaltet sein.
      */
-    //% block
+    //% blockId=vorneFrei block="vorne frei"
+    export function vorneFrei(): boolean {
+        if (eingeschaltet) {
+            if (richtung == 0 && y > 0) {
+                return true
+            } else if (richtung == 1 && x < 4) {
+                return true
+            } else if (richtung == 2 && y < 4) {
+                return true
+            } else if (richtung == 3 && x > 0) {
+                return true
+            } else {
+                return false
+            }
+        } else {
+            fehler = true
+            basic.showIcon(IconNames.Sad)
+            return false
+        }
+    }
+
+    /**
+     * Liefert "true", wenn Niki nach Norden blickt. Niki muss eingeschaltet sein.
+     */
+    //% blockId=nordwaerts block="nordwärts"
+    export function nordwaerts(): boolean {
+        if (eingeschaltet) {
+            return (richtung == 0)
+        } else {
+            fehler = true
+            basic.showIcon(IconNames.Sad)
+            return false
+        }
+    }
+
+    /**
+     * Liefert "true", wenn Niki nach Osten blickt. Niki muss eingeschaltet sein.
+     */
+    //% blockId=ostwaerts block="ostwärts"
+    export function ostwaerts(): boolean {
+        if (eingeschaltet) {
+            return (richtung == 1)
+        } else {
+            fehler = true
+            basic.showIcon(IconNames.Sad)
+            return false
+        }
+    }
+
+    /**
+     * Liefert "true", wenn Niki nach Süden blickt. Niki muss eingeschaltet sein.
+     */
+    //% blockId=suedwaerts block="südwärts"
+    export function suedwaerts(): boolean {
+        if (eingeschaltet) {
+            return (richtung == 2)
+        } else {
+            fehler = true
+            basic.showIcon(IconNames.Sad)
+            return false
+        }
+    }
+
+    /**
+     * Liefert "true", wenn Niki nach Westen blickt. Niki muss eingeschaltet sein.
+     */
+    //% blockId=westwaerts block="westwärts"
+    export function westwaerts(): boolean {
+        if (eingeschaltet) {
+            return (richtung == 3)
+        } else {
+            fehler = true
+            basic.showIcon(IconNames.Sad)
+            return false
+        }
+    }
+
+    /**
+     * Nimm eine Schraube von der aktuellen Position auf. Bei mehreren Schrauben auf der Position wird nur eine aufgehoben. Niki darf nichts von einem leeren Feld aufheben und muss eingeschaltet sein.
+     */
+    //% blockId=nimmAuf block="nimm auf"
     export function nimmAuf(): void {
         if (eingeschaltet) {
-            basic.pause(geschwindigkeit)
+            basic.pause(verzoegerung)
             if (spielfeld[x][y] > 0) {
                 vorrat++
                 spielfeld[x][y]--
+            } else {
+                fehler = true
+                basic.showIcon(IconNames.Sad)
+                ausschalten()
             }
         } else {
             basic.showIcon(IconNames.Sad)
@@ -273,55 +312,77 @@ namespace niki {
     }
 
     /** 
-     * Lege ein Token auf die aktuelle Position, sofern Nikis Vorrat größer als 0 ist.
+     * Lege eine Schraube auf die aktuelle Position. Nikis Vorrat muss größer als 0 sein und Niki muss eingeschaltet sein.
      */
-    //% block
+	//% blockId=legeAb block="lege ab"
     export function legeAb(): void {
         if (eingeschaltet) {
-            basic.pause(geschwindigkeit)
+            basic.pause(verzoegerung)
             if (vorrat > 0) {
                 spielfeld[x][y]++
                 vorrat--
+            } else {
+                fehler = true
+                basic.showIcon(IconNames.Sad)
+                ausschalten()
             }
         } else {
             basic.showIcon(IconNames.Sad)
         }
     }
 
+
     /**
-    * Liefert "true", wenn sich an Nikis aktueller Position ein Token befindet.
-    */
-    //% block
+     * Liefert "true", wenn sich an Nikis aktueller Position eine Schraube befindet. Niki muss eingeschaltet sein.
+     */
+    //% blockId=platzBelegt block="Platz belegt"
     export function platzBelegt(): boolean {
-        return (spielfeld[x][y] > 0)
+        if (eingeschaltet) {
+            return (spielfeld[x][y] > 0)
+        } else {
+            fehler = true
+            basic.showIcon(IconNames.Sad)
+            return false
+        }
     }
 
     /**
-     * Liefert "true", wenn Niki mindestens ein Token aufgenommen (und nicht wieder abgelegt) hat.
+     * Liefert "true", wenn Nikis Vorrat größer als 0 ist. Niki muss eingeschaltet sein.
      */
-    //% block
+    //% blockId=hatVorrat block="hat Vorrat"
     export function hatVorrat(): boolean {
-        return (vorrat > 0)
+        if (eingeschaltet) {
+            return (vorrat > 0)
+        } else {
+            fehler = true
+            basic.showIcon(IconNames.Sad)
+            return false
+        }
     }
 
     /**
-     * Setzt ein Delay von n Millisekunden nach jeder Aktion.
+     * Setze eine Verzögerung von n Millisekunden nach jeder Aktion.
      */
-    //% block
-    export function setzeGeschwindigkeit(n: number): void {
-        geschwindigkeit = n
+    //% blockId=setzeVerzoegerung block="setze Verzögerung %verzoegerung_neu"
+    export function setzeVerzoegerung(verzoegerung_neu: number): void {
+        if (verzoegerung_neu >= 0) {
+            verzoegerung = verzoegerung_neu
+        }
     }
 
     /**
-    * Schaltet Niki aus.
-    */
-    //% block
+     * Schalte Niki aus. Niki muss eingeschaltet sein.
+     */
+    //% blockId=ausschalten block="schalte aus"
     export function ausschalten(): void {
         if (eingeschaltet) {
             if (spielfeld[x][y] == 0) {
                 led.unplot(x, y)
             }
             eingeschaltet = false;
+        } else {
+            fehler = true
+            basic.showIcon(IconNames.Sad)
         }
     }
-} 
+}
